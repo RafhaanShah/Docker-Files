6ba7891a943ae3a2bd1aef479e3ac61be819bfe3
Rafhaan Shah <RafhaanShah@users.noreply.github.com>
2022-01-07 22:22:20 +0000

    Update misc

--- .git-hooks/setup.sh
+++ .git-hooks/setup.sh
@@ -15,8 +15,8 @@ fi
 git config core.hooksPath ".git-hooks"
 chmod -R +x "$(git rev-parse --git-path hooks)"
 
-cp "paths.txt.example" "paths.txt"
-sed -i 's|HOME_DIR|'${HOME}'|' "paths.txt"
+cp "$(git rev-parse --git-path hooks)/paths.txt.example" "$(git rev-parse --git-path hooks)/paths.txt"
+sed -i 's|HOME_DIR|'${HOME}'|' "$(git rev-parse --git-path hooks)/paths.txt"
 
 npm install --global prettier
 pip3 install --user yamllint
