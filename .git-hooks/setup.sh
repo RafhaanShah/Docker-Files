d181aebcb441ef9928591def5d43a5148ec53c5d
Rafhaan Shah <RafhaanShah@users.noreply.github.com>
2025-07-15 22:17:22 +0000

    Update hooks

--- .git-hooks/setup.sh
+++ .git-hooks/setup.sh
@@ -2,21 +2,5 @@
 
 set -e
 
-if ! [ -x "$(command -v npm)" ]; then
-  echo 'Error: npm is not installed' >&2
-  exit 1
-fi
-
-if ! [ -x "$(command -v pip3)" ]; then
-  echo 'Error: pip3 is not installed' >&2
-  exit 1
-fi
-
 git config core.hooksPath ".git-hooks"
 chmod -R +x "$(git rev-parse --git-path hooks)"
-
-cp "$(git rev-parse --git-path hooks)/paths.txt.example" "$(git rev-parse --git-path hooks)/paths.txt"
-sed -i 's|HOME_DIR|'${HOME}'|' "$(git rev-parse --git-path hooks)/paths.txt"
-
-npm install --global prettier
-pip3 install --user yamllint
--- README.md
+++ README.md
@@ -3,5 +3,11 @@
 Docker files for stuff I use.
 
 ## Setup:
+
 - Update `daemon.json`: `mkdir -p ~/.config/docker; cp daemon.json ~/.config/docker/daemon.json`
 - Create external network: `docker network create docker`
+
+## Validate and Format
+
+- `git ls-files '*.yml' | xargs yamllint`
+- `git ls-files '*.yml' | xargs prettier --write --no-bracket-spacing`
